303ea4d3a4be7934d361241da42bebd49619ca84
isac7722 <isac7722@gmail.com>
2023-12-11 18:15:54 +0900

    11Dec

---
+++ chap08-security-session/src/main/java/com/ohgiraffers/securitysession/auth/controller/IndexController.java
@@ -0,0 +1,25 @@
+package com.ohgiraffers.securitysession.auth.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class IndexController {
+    @GetMapping
+    public String root(){
+        return "index";
+    }
+
+    @GetMapping("/admin/page")
+    public ModelAndView admin(ModelAndView modelAndView){
+        modelAndView.setViewName("admin/admin");
+        return modelAndView;
+    }
+
+    @GetMapping("/user/page")
+    public ModelAndView user(ModelAndView modelAndView){
+        modelAndView.setViewName("user/user");
+        return modelAndView;
+    }
+}
